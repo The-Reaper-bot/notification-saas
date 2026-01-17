1098147d86a579238524dda52d1432b89d38c6cb
Rajat Samant <Rjtsngsmt@gmail.com>
2026-01-17 22:27:25 +0530

    Refactor user model and update authentication logic to use pluralized 'Users' class

--- src/main/java/com/saas/notification/user/UserRepository.java
+++ src/main/java/com/saas/notification/user/UserRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository extends JpaRepository<User, UUID> {
+public interface UserRepository extends JpaRepository<Users, UUID> {
 
-    Optional<User> findByEmail(String email);
+    Optional<Users> findByEmail(String email);
 }
